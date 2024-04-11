e04723b78562d7c02d1c009ad502d8636666179d
Darko Ristevski <dareeco@outlook.com>
2024-04-11 09:02:19 +0200

    Image added in the details modal

--- src/components/detailsDialog.js
+++ src/components/detailsDialog.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 const DetailsDialog = ({ pet, onClose }) => {
+  //fetch again
   return (
     <div className="bg-white w-96 p-4 rounded-lg shadow-lg">
       <h2 className="text-lg font-bold mb-4">Pet Details</h2>
       <div>
         <p>Name: {pet.name}</p>
         <p>Description: {pet.description}</p>
+        <img
+          alt="Sorry, there isn't photo for this pet"
+          width={"256px"}
+          height={"256px"}
+          src={`http://localhost:8080/pet/${pet.id}/image`}
+        />
       </div>
       <button
         onClick={onClose}
